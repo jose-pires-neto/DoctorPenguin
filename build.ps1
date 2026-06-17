cacfdb649b590240a2c131e864d2d7d94f6bb1fc
jose-pires-neto <jose88pires@hotmail.com>
2026-06-17 17:48:00 -0300

    feat: generate and set professional icon for pyinstaller executable

--- build.ps1
+++ build.ps1
@@ -7,7 +7,7 @@ pip install pyinstaller
 
 # 2. Compila o projeto
 Write-Host "Compilando o arquivo executável..." -ForegroundColor Green
-pyinstaller --noconsole --onefile --name "DoctorPenguin" --add-data "penguin_sprites_aligned.png;." main.py
+pyinstaller --noconsole --onefile --name "DoctorPenguin" --add-data "penguin_sprites_aligned.png;." --icon "icon.ico" --hidden-import "pyttsx3.drivers" --hidden-import "pyttsx3.drivers.sapi5" --hidden-import "edge_tts" --hidden-import "aiohttp" main.py
 
 Write-Host "Concluído! O seu executável está na pasta 'dist'." -ForegroundColor Green
 pause
